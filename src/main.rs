469c58165f2d5897476f200f68b3bed7e0a80f33
andreasbaumgartner <baumgartner.andy@hotmail.ch>
2023-06-04 00:18:07 +0200

    add template_tera and handlers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+#[get("/world")]
+fn index() -> &'static str {
+    "Hello, world!";
+    "please go to /test"
+}
+
+#[get("/test")]
+fn hello_test() -> &'static str {
+    "Hello, test!"
+}
+
+#[get("/<name>")]
+fn hello(name: &str) -> String {
+    format!("Hello, {}!", name)
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/", routes![hello_test])
+        .mount("/hello", routes![hello])
 }
